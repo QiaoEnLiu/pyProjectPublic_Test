9c3962f5bf28057c0946b94832f243b35fee9bc1
QiaoEnLiu <137969620+QiaoEnLiu@users.noreply.github.com>
2024-02-06 16:04:17 +0800

    2024 0206 1605

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # main.py
-# main.py叫sub.py修改shared_module.py的global_variable
-# 然後main.py讀取shared_module.py的global_variable
+# Step 1：main.py先讀取shared_module.py的global_variable
+# Step 2：main.py叫sub.py修改shared_module.py的global_variable
+    # Step 2.1：sub.py對shared_module.py的global_variable進行修改
+# Step 3：然後main.py再讀取shared_module.py的global_variable
 
 # 匯入共享模組
 import shared_module
@@ -8,14 +10,14 @@ from sub import modify_global_variable
 
 def main():
     # 在函數內部使用全域變數
-    print("main.py 讀取 shared_module.py 的 global_variable：", shared_module.global_variable)
+    print("Step 1 ", shared_module.global_variable)
 
-    print("main.py 叫 sub.py 修改 shared_module.py 的 global_variable。")
+    print("Step 2 Modify")
     # 呼叫 sub.py 中的函數修改 global_variable
     modify_global_variable()
 
     # 再次印出修改後的 global_variable
-    print("main.py 讀取 shared_module.py 的 global_variable：", shared_module.global_variable)
+    print("Step 3：", shared_module.global_variable)
 
 # 如果需要在 main.py 中執行一些程式碼，可以放在這裡
 if __name__ == "__main__":
--- sub.py
+++ sub.py
@@ -5,7 +5,5 @@ import shared_module
 
 def modify_global_variable():
     # 在 sub.py 中修改 shared_module.py 的 global_variable
-    # global global_variable
     shared_module.global_variable = 15
-    print("在 sub.py 中修改 shared_module.py 的 global_variable為：", shared_module.global_variable)
-    # return shared_module.global_variable
+    print("Step 2.1：", shared_module.global_variable)
